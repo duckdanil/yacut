e3f32c7fa6cbcb8d844ac8471a7d2c858a679974
Данил Уткин <duck.danil@yandex.ru>
2023-06-30 13:38:41 +0300

    pep

--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ import os
 
 LENGTH_SHORT_ID = 6
 
+
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
